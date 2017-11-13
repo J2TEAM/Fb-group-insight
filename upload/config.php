e447ba704d15c0c6854a44efa1df475c324f7ac3
T-Rekt <t.to.the.rekt@gmail.com>
2017-11-13 18:51:28 +0700

    Seperate config.php

---
+++ upload/config.php
@@ -0,0 +1,9 @@
+<?php
+	// DON'T EDIT, PLEASE RESPECT COPYRIGHT
+  define('AUTHOR', 'T-Rekt');
+  define('COPYRIGHT', 'J2TEAM');
+
+  // EDIT THIS
+  define('token', '');
+  define('gid', '');
+?>
--- upload/index.php
+++ upload/index.php
@@ -1,11 +1,5 @@
 <?php
-  // DON'T EDIT, PLEASE RESPECT COPYRIGHT
-  define('AUTHOR', 'T-Rekt');
-  define('COPYRIGHT', 'J2TEAM');
-
-  // EDIT THIS
-  define('token', '');
-  define('gid', '');
+  include("config.php");
 ?>
 
 <?php
